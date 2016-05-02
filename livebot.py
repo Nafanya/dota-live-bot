4a34c095351fbcb4b074e8809222d2a97e087687
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2016-05-03 01:19:52 +0300

    Add image rendering to bot

--- livebot.py
+++ livebot.py
@@ -1,38 +1,41 @@
-import time
-import telepot
-import logging
 import datetime
+import logging
+import time
 
-from livegames import get_live_games_stats
+import telepot
+from config.tokens.tokens import TELEGRAM_TOKEN
+from livegames import get_img_live_game_stats, get_live_games_stats
 from upcoming import get_upcoming
 
 
 def handle(msg):
     logger.info(msg['text'] + ' from ' + str(msg['chat']['id']) + ' @ ' + str(datetime.datetime.now()))
-    if msg['text'] == '/live':
+    text = msg['text']
+    if text == '/start':
+        bot.sendMessage(int(msg['chat']['id']), 'Hi! Send /live to see live matches, /liveimg to see them as images and /upcoming for upcoming games')
+    elif text == '/live':
         for game in get_live_games_stats():
             bot.sendMessage(int(msg['chat']['id']), game)
-    elif msg['text'] == '/upcoming':
+    elif text == '/liveimg':
+        for game_img in get_img_live_game_stats():
+            with open(game_img, 'rb') as photo:
+                bot.sendPhoto(int(msg['chat']['id']), photo)
+    elif text == '/upcoming':
         url = 'http://wiki.teamliquid.net/dota2/WePlay_Dota2_League/Season_3'
         for game in get_upcoming(url):
             bot.sendMessage(int(msg['chat']['id']), game)
 
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    handler = logging.FileHandler('bot.log')
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
 
-def load_token():
-    with open('config/tokens/telegram', 'r') as token_file:
-        return token_file.read().replace('\n', '')
+    bot = telepot.Bot(TELEGRAM_TOKEN)
+    bot.message_loop(handle)
+    print ('Listening ...')
 
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-handler = logging.FileHandler('bot.log')
-handler.setLevel(logging.INFO)
-logger.addHandler(handler)
-
-bot = telepot.Bot(load_token())
-bot.message_loop(handle)
-print ('Listening ...')
-
-while True:
-    time.sleep(10)
+    while True:
+        time.sleep(10)
--- livegames.py
+++ livegames.py
@@ -1,9 +1,15 @@
+import json
+import subprocess
+
 import dota2api
+import os
+import requests
+
+from config.tokens.tokens import STEAM_TOKEN
 
 
-def load_token():
-    with open('config/tokens/steam', 'r') as token_file:
-        return token_file.read().replace('\n', '')
+with open('templates/base.html') as f:
+    TEMPLATE = f.read()
 
 
 def get_important_leagues():
@@ -21,7 +27,7 @@ def get_abbreviations():
 
 
 def get_time(game_id):
-    minutes = int(game_id['scoreboard']['duration']) / 60
+    minutes = int(game_id['scoreboard']['duration']) // 60
     seconds = int(game_id['scoreboard']['duration']) % 60
     return str(minutes).zfill(2) + ':' + str(seconds).zfill(2)
 
@@ -101,7 +107,7 @@ def get_game_info(game_id):
 
 
 def get_live_games_stats():
-    api = dota2api.Initialise(load_token())
+    api = dota2api.Initialise(STEAM_TOKEN)
     live_games = api.get_live_league_games()
     stats = []
     for game in live_games['games']:
@@ -110,3 +116,91 @@ def get_live_games_stats():
     if len(stats) == 0:
         stats.append('Sorry, no live games found.')
     return stats
+
+
+def get_team_image_url(game_id, side):
+    img_url = 'http://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/?key={key}&appid=570&ugcid={team_logo}'
+    try:
+        team_logo = game_id[side + '_team']['team_logo']
+    except KeyError:
+        return 'No image'
+    response = requests.get(img_url.format(key=STEAM_TOKEN, team_logo=team_logo))
+    print(response.text)
+    jsn = json.loads(response.text)
+    try:
+        return jsn['data']['url']
+    except KeyError:
+        return 'No image'
+
+
+def get_team_images(game_id):
+    radiant = get_team_image_url(game_id, 'radiant')
+    dire = get_team_image_url(game_id, 'dire')
+    return {
+        'radiant': radiant,
+        'dire': dire
+    }
+
+
+# Same as get_leads but without time
+def get_leads2(game_id):
+    nw_radiant, nw_dire = get_networth(game_id)
+    xp_radiant, xp_dire = get_xp(game_id)
+    nw_diff = str(abs(nw_radiant - nw_dire))
+    xp_diff = str(abs(xp_radiant - xp_dire))
+    if nw_radiant > nw_dire:
+        if xp_radiant > xp_dire:
+            text = 'radiant leads by ' + nw_diff + ' gold and ' + xp_diff + ' XP'
+        else:
+            text = 'radiant leads by ' + nw_diff + ' gold, dire leads by ' + xp_diff + ' XP'
+    else:
+        if nw_dire > nw_radiant:
+            text = 'dire leads by ' + nw_diff + ' gold and ' + xp_diff + ' XP'
+        else:
+            text = 'dire leads by ' + nw_diff + ' gold, radiant leads by ' + xp_diff + ' XP'
+    return text
+
+
+def render_game(game_id):
+    try:
+        game_id['scoreboard']
+    except KeyError:
+        return '> Whoops.'
+
+    match_id = game_id['match_id']
+    radiant = get_team_name(game_id, 'radiant')
+    dire = get_team_name(game_id, 'dire')
+    duration = int(game_id['scoreboard']['duration'])
+    if duration == 0:
+        game_time = 'drafting'
+    else:
+        game_time = get_time(game_id)
+    images = get_team_images(game_id)
+    output_path = 'templates/responses/{}.png'.format(match_id)
+    output_html = 'templates/responses/{}.html'.format(match_id)
+    with open(output_html, 'w') as out:
+        out.write(TEMPLATE
+                  .replace('RADIANT_NAME', radiant)
+                  .replace('DIRE_NAME', dire)
+                  .replace('GAME_TIME', game_time)
+                  .replace('GAME_KILLS', get_kills(game_id))
+                  .replace('GAME_LEADS', get_leads2(game_id))
+                  .replace('RADIANT_IMG', images['radiant'])
+                  .replace('DIRE_IMG', images['dire']))
+    subprocess.call(['python2', 'python-webkit2png/webkit2png/scripts.py', '-o', output_path, '-g', '550', '150', output_html])
+    return output_path
+
+
+def get_img_live_game_stats():
+    api = dota2api.Initialise(STEAM_TOKEN)
+    live_games = api.get_live_league_games()
+    important_leagues = get_important_leagues()
+    stats = list()
+    for game in live_games['games']:
+        if game['league_id'] in important_leagues or game['spectators'] > 100:
+            stats.append(render_game(game))
+    return stats
+
+
+if __name__ == '__main__':
+    print(get_img_live_game_stats())
